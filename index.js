25e5f26a0a4569939128c1d472c6a1f1a45b6a60
Jessica Vaughn <jessica.vaughn619@gmail.com>
2022-11-14 14:11:36 -0700

    Solution

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (typeof activity == "string") {
+        return "This Saturday, I want to " + activity + "!"
+    } else {
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+let mondayWork = function (activity) {
+    if (typeof activity !== "undefined") {
+        return "This Monday, I will " + activity + "."
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(flair) {
+    return function(important) {
+        if (typeof important !== "undefined") {
+            return `You are ${flair}${important}${flair}!`
+    } else {
+        return "You are special!"
+    }
+    }}
